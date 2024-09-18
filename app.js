8c23df2497269a53d022e88acddc0c759f32d5ab
adamctr <kuradakun@gmail.com>
2024-09-18 13:41:14 +0200

    first draft

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const config = require("./config/config.json");
+
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  config.development.database,
+  config.development.username,
+  config.development.password,
+  {
+    host: config.development.host,
+    dialect: config.development.dialect,
+  }
+);
+
+// Tester la connexion
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connexion réussie à la base de données.");
+  })
+  .catch((err) => {
+    console.error("Impossible de se connecter :", err);
+  });
+
+// Démarrer le serveur
+app.listen(3000, () => {
+  console.log("Serveur démarré sur le port 3000");
+});
