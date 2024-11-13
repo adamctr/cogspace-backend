52cc9de41cbdaf87ed542c4c40153881b946e0c5
Alexis <garibaldialexis@gmail.com>
2024-11-13 14:17:32 +0100

    hotfixes

--- app.js
+++ app.js
@@ -27,12 +27,14 @@ const ficheRoutes = require("./routes/ficheRoutes");
 const categorieRoutes = require("./routes/categorieRoutes");
 const utilisateurRoutes = require("./routes/utilisateurRoutes");
 const commentaireRoutes = require("./routes/commentaireRoutes");
+const roleRoutes = require("./routes/roleRoutes");
 
 // Routes
 app.use("/post", ficheRoutes); // Routes pour les fiches
 app.use("/category", categorieRoutes); // Routes pour les catégories
 app.use("/user", utilisateurRoutes); // Routes pour les utilisateurs
 app.use("/comment", commentaireRoutes); // Routes pour les utilisateurs
+app.use("/role", roleRoutes); // Routes pour les rôles
 
 // Gérer les erreurs 404
 app.use((req, res, next) => {
